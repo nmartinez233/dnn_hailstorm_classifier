b7ed2ae59ef0786827fc6cfca7b7a64f990b2108
Nathaniel Martinez <nmartinez23@uchicago.edu>
2020-07-22 16:28:52 -0500

    Upload Working Autoencoder

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Flatten
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, Activation
 from keras.models import Model, load_model
 from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
@@ -7,56 +7,61 @@ import matplotlib.pyplot as plt
 import pyart
 import glob
 
+"""
+input_img = Input(shape=(256, 256, 3))
 
-input_img = Input(shape=(200, 200, 3))
-
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = Conv2D(64, (3, 3), padding='same')(input_img)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
 x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(32, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
 x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(16, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = Conv2D(16, (3, 3), padding='same')(encoded)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(32, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(64, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
+x = Activation('relu')(x)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-decoded = Flatten()(x)
+x = Conv2D(3, (3, 3), padding='same')(x)
+x = BatchNormalization()(x)
+decoded = Activation('sigmoid')(x)
 
 autoencoder = Model(input_img, decoded)
-autoencoder.compile(optimizer='adadelta', loss='mean_squared_error')
-
-#autoencoder = load_model("autoencoder_model.h5")
+autoencoder.compile(optimizer='adam', loss='mse')
+"""
+autoencoder = load_model("autoencoder_modelmse.h5")
 
 #train the model
 train_datagen = ImageDataGenerator(
         rescale=1./255,
-        validation_split=0.2,
-        featurewise_std_normalization=True,
-        featurewise_center=True)
+        validation_split=0.2)
 
 x_train = train_datagen.flow_from_directory(
         '../data/images/',
-        target_size=(200, 200),
+        target_size=(256, 256),
         batch_size=10,
-        class_mode='binary')
+        class_mode='input')
 x_test = train_datagen.flow_from_directory(
         '../data/images/',
-        target_size=(200, 200),
+        target_size=(256, 256),
         batch_size=10,
-        class_mode='binary')
+        class_mode='input')
 
 autoencoder.summary()
-autoencoder.fit(
-        x=x_train,
-        batch_size=10,
-        epochs=50,
-        validation_data=x_test,
-        verbose=1)
+autoencoder.fit(x=x_train, batch_size=10, epochs=85, validation_data=x_test, verbose=1, shuffle=True)
 
 print("\nFinished Training... \n")
 
@@ -64,8 +69,6 @@ print("\nFinished Training... \n")
 autoencoder.save('autoencoder_modelmse.h5')
 
 print("\nFinished Saving... \n")
-
-
 
 #show the before and after images
 i = 0
@@ -79,12 +82,12 @@ plt.figure(figsize=(4, 4))
 img = x_test[i][i]
 #original
 ax = plt.subplot(2, n, i + 1)
-plt.imshow(img[i].reshape(200, 200, 3))
+plt.imshow(img[i].reshape(256, 256, 3))
 ax.set_axis_off()
 
     #reconstruction
 ax = plt.subplot(2, n, i + n + 1)
-plt.imshow(decoded_imgs[i].reshape(200, 200), cmap='pyart_HomeyerRainbow')
+plt.imshow(decoded_imgs[i].reshape(256, 256, 3), cmap='pyart_HomeyerRainbow')
 ax.set_axis_off()
 plt.show()
 K.clear_session()
--- autoencoder/unet-autoencoder.py
+++ autoencoder/unet-autoencoder.py
@@ -9,38 +9,40 @@ import pyart
 import glob
 
 input_shape = (256, 256, 3)
-autoencoder = custom_unet(input_shape=input_shape, output_activation='sigmoid')
+autoencoder = custom_unet(input_shape=input_shape, output_activation='sigmoid', num_classes=3)
 autoencoder.compile(optimizer='adadelta', loss='mean_squared_error')
 
 #train the model
 train_datagen = ImageDataGenerator(
         rescale=1./255,
         validation_split=0.2,
-        featurewise_std_normalization=True,
-        featurewise_center=True)
+        featurewise_std_normalization=True)
 
 x_train = train_datagen.flow_from_directory(
         '../data/images/',
         target_size=(256, 256),
-        batch_size=10,
-        class_mode='binary')
+        batch_size=1,
+        class_mode='input')
 x_test = train_datagen.flow_from_directory(
         '../data/images/',
         target_size=(256, 256),
-        batch_size=10,
-        class_mode='binary')
+        batch_size=1,
+        class_mode='input')
+
+#autoencoder = load_model("256x256-model.h5")
+
 
 autoencoder.summary()
-autoencoder.fit(
-        x=x_train,
-        batch_size=10,
-        epochs=50,
-        validation_data=x_test,
-        verbose=1)
+#autoencoder.fit(
+ #       x=x_train,
+  #      batch_size=1,
+   #     epochs=50,
+    #    validation_data=x_test,
+     #   verbose=1)
 
 print("\nFinished Training... \n")
 
-autoencoder.save("256x256-model.h5")
+#autoencoder.save("256x256-model.h5")
 
 print("\nFinished Saving... \n")
 
@@ -52,16 +54,17 @@ print("Minimum:", np.min(decoded_imgs))
 print("Maximum:", np.max(decoded_imgs))
 n = 2
 
-plt.figure(figsize=(4, 4))
+plt.figure(figsize=(5, 5))
 img = x_test[i][i]
 #original
 ax = plt.subplot(2, n, i + 1)
-plt.imshow(img[i].reshape(256, 256, 3))
+plt.imshow(img[i].reshape(256, 256, 3), cmap="Greys", vmin=-32, vmax=64)
 ax.set_axis_off()
 
     #reconstruction
 ax = plt.subplot(2, n, i + n + 1)
-plt.imshow(decoded_imgs[i].reshape(256, 256), cmap='pyart_HomeyerRainbow')
+corrected_images = decoded_imgs.astype(np.float64)
+plt.imshow(corrected_images[i].reshape(256, 256, 3), cmap="Greys", vmin=-32, vmax=64)
 ax.set_axis_off()
 plt.show()
 K.clear_session()
