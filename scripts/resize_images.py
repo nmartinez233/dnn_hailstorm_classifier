d71402e914cadb293905a5c64e1fd0138a14b0ab
Nathaniel Martinez <nmartinez23@uchicago.edu>
2020-07-22 16:27:30 -0500

    Uploading 256x256 images

--- scripts/resize_images.py
+++ scripts/resize_images.py
@@ -1,14 +1,14 @@
 from PIL import Image                                              
 import os, sys                       
 
-path = "../data/images/smaller_filtered_pngs/"
+path = "../data/images/256_pngs/"
 dirs = os.listdir( path )                                       
 
 def resize():
     for item in dirs:
         im = Image.open(path+item)
         f, e = os.path.splitext(path+item)
-        imResize = im.resize((200,200), Image.ANTIALIAS)
+        imResize = im.resize((256,256), Image.ANTIALIAS)
         imResize.save(f+'.png', 'png', quality=80)
 
 resize()
