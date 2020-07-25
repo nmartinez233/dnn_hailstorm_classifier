d6d4515ca93a3a79bf615e4bb6d68e330f529b24
Nathaniel Martinez <nmartinez@beboplogin3.lcrc.anl.gov>
2020-07-24 19:37:01 -0500

    Uploading changes to png scripts

--- scripts/make_pngs_dask.py
+++ scripts/make_pngs_dask.py
@@ -6,9 +6,12 @@ import numpy as np
 import multiprocessing
 import matplotlib.pyplot as plt
 
+from distributed import Client, wait
+from dask_jobqueue import SLURMCluster
 from matplotlib import use
 from datetime import datetime
 use('agg')
+
 
 def make_plot(filename):
     try:
@@ -23,15 +26,14 @@ def make_plot(filename):
     len_gt20 = gt20.size
     len_total = total.size
     pcoverage = len_gt20/len_total
-    
+
     if pcoverage == 1.:
         decision = 1
     else:
         decision = np.random.choice([0,1], p=[1.-pcoverage, pcoverage])
 
     if decision == 1:
-
-        # Parse the scan time
+        ## Parse the scan time
         rad_time = datetime.strptime(radar_object.time['units'], 'seconds since %Y-%m-%dT%H:%M:%SZ')
 
         # Apply mask here
@@ -53,7 +55,7 @@ def make_plot(filename):
         fig.savefig('%s%s' % (png_out_dir, rad_time.strftime('%Y%m%d-%H%M%S.png')), dpi=300)
         plt.close(fig)
         print(filename, "processed!")
-    
+        print("Generated png for time %s" %  rad_time.strftime('%Y%m%d-%H%M%S.png'))
         # Free memory
         del radar_object.fields
         del radar_object
@@ -64,6 +66,8 @@ def make_plot(filename):
         del bottom_sweep
         del total 
         del gt20
+
+        return
 
     else:
         print(filename, "skipped!")
@@ -77,15 +81,25 @@ def make_plot(filename):
 
         return #do not create image
 
-file_directory = '../data/NEXRAD2/*V06*'
+file_directory = '../data/NEXRAD/*'
 file_list = glob.glob(file_directory)
-png_out_dir = '../data/images/filtered_pngs/'
+print(file_list)
+png_out_dir = '../data/images/full_pngs/'
 i = 0
 use('agg')
 
 if __name__ == "__main__":
-    for filename in file_list:
-        make_plot(filename)
-        i += 1
-        if i % 10 == 0:
-            print("File %d of %d processed" % (i, len(file_list)))
+    #for filename in file_list:
+    #    make_plot(filename)
+    #    i += 1
+    #    if i % 10 == 0:
+    #        print("File %d of %d processed" % (i, len(file_list)))
+    Cluster = SLURMCluster(processes=6, cores=36, memory='128GB', walltime='2:00:00')
+    Cluster.scale(36)
+    client = Client(Cluster)
+    print("Waiting for workers...")
+    while(len(client.scheduler_info()["workers"]) < 6):
+        i = 1
+    futures = client.map(make_plot, file_list)
+    wait(futures)
+    client.close() 
